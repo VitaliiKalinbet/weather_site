e9b213b4c953a2cb3270f304f41a4c6a2999e4fb
VitaliiKalinbet <bosssvit@gmail.com>
2019-01-24 14:43:52 +0200

    Updates

---
+++ precache-manifest.b808614ad385dacffdd7f2a66e9b7ede.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2d4d64ce179c3fac8d94dc7639f4aff",
+    "url": "/weather_site/static/media/canyon.e2d4d64c.jpg"
+  },
+  {
+    "revision": "011d30a886c6877627d11f3654024872",
+    "url": "/weather_site/static/media/calendar.011d30a8.png"
+  },
+  {
+    "revision": "e89dd98416616f3b893d",
+    "url": "/weather_site/static/js/runtime~main.e89dd984.js"
+  },
+  {
+    "revision": "1c97e6c040c04dc14f7a",
+    "url": "/weather_site/static/js/main.1c97e6c0.chunk.js"
+  },
+  {
+    "revision": "4abaa156df93ecc634a4",
+    "url": "/weather_site/static/js/1.4abaa156.chunk.js"
+  },
+  {
+    "revision": "1c97e6c040c04dc14f7a",
+    "url": "/weather_site/static/css/main.323b21b6.chunk.css"
+  },
+  {
+    "revision": "4abaa156df93ecc634a4",
+    "url": "/weather_site/static/css/1.99bcccf7.chunk.css"
+  },
+  {
+    "revision": "622b63fbb8d844e92606090ce2bbb837",
+    "url": "/weather_site/index.html"
+  }
+];
